710bd74ef678dfa57ecac648c0908fb5b349f31f
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2020-05-21 02:16:53 +0530

    chore: Multi-class components files split into seperate files

---
+++ client/components/SideNav/NavListItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class NavListItem extends Component {
+    render() {
+        const { listItem, active, urlPrefix } = this.props;
+        return (
+            <NavLink
+                title={listItem.title || listItem.name}
+                to={`${urlPrefix}/${listItem._id}`}
+                activeClassName="active-workspace"
+                className={"nav-list-item text-white"}
+            >
+                <span className="nav-list-item--lastactive">
+                    <i className={`fas fa-circle ${active ? "text-success" : ""}`}></i>
+                </span>
+                <span className="nav-list-item--name ellipsis">{listItem.title || listItem.name}</span>
+            </NavLink>
+        );
+    }
+}
+
+export default NavListItem;
---
+++ client/constants/constants.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-useless-escape */
+
+// eslint-disable-next-line import/prefer-default-export
+export const mailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
