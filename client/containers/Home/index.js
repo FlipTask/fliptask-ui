a21f7dd3c1f6a7c5b12b0b76c6c9686d587a72c2
Restyled.io <commits@restyled.io>
2020-06-02 15:32:57 +0000

    Restyled by clang-format

--- client/containers/Home/index.js
+++ client/containers/Home/index.js
@@ -1,40 +1,34 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router";
-import SideNav from "../../components/SideNav";
-import RenderRoutes from "../../components/RenderRoutes";
-import { fetchBoards, fetchUser } from "../../actions";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {withRouter} from "react-router";
+
+import {fetchBoards, fetchUser} from "../../actions";
 import BoardHeader from "../../components/Header/BoardHeader";
+import RenderRoutes from "../../components/RenderRoutes";
+import SideNav from "../../components/SideNav";
 
 class Home extends Component {
-    componentDidMount() {
-        this.props.fetchBoards();
-        this.props.fetchUser();
-    }
+  componentDidMount() {
+    this.props.fetchBoards();
+    this.props.fetchUser();
+  }
 
-    render() {
-        const {
-            activeWorkspace
-        } = this.props;
-        return (
-            <React.Fragment>
-                <SideNav />
-                <div className="board--wrapper">
-                    <BoardHeader board={ activeWorkspace }/>
+  render() {
+    const {activeWorkspace} = this.props;
+    return (
+        <React.Fragment><SideNav /><div className = "board--wrapper">
+        <BoardHeader board =
+         {
+           activeWorkspace
+         } />
                     <RenderRoutes routes={this.props.route.routes} />
-                </div>
-            </React.Fragment>
-        );
-    }
+        </div>
+            </React.Fragment>);
+  }
 }
 
-const mapStateToProps = ({ user, boards }) => ({
-    activeWorkspace: boards.activeBoard,
-    workspaces: boards.boards,
-    user
-});
+const mapStateToProps = ({user, boards}) =>
+    ({activeWorkspace : boards.activeBoard, workspaces : boards.boards, user});
 
-export default withRouter(connect(mapStateToProps, {
-    fetchBoards,
-    fetchUser
-})(Home));
+export default withRouter(
+    connect(mapStateToProps, {fetchBoards, fetchUser})(Home));
