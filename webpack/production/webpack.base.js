768d181fd3f9cbea176607b74cb066d6f0f59d88
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2020-06-03 01:31:46 +0530

    chore: svg loader added in webpack prod config

--- webpack/production/webpack.base.js
+++ webpack/production/webpack.base.js
@@ -16,6 +16,19 @@ module.exports = {
                 options: {
                     presets: ["es2015", "react", "stage-2"]
                 }
+            },{
+                test: /\.svg$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    }, {
+                        loader: "react-svg-loader",
+                        options: {
+                            jsx: true // true outputs JSX tags
+                        }
+                    }
+                ]
             }
         ]
     },
